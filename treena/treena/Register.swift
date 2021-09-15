4984bd8e491d8acf41a5330ad569a73412eb411b
asong57 <1998thddk@gmail.com>
2021-09-15 14:43:17 +0900

    feat : 회원가입 정보 파이어베이스에 저장

--- treena/treena/Register.swift
+++ treena/treena/Register.swift
@@ -7,16 +7,34 @@
 
 import Foundation
 import Firebase
+import FirebaseDatabase
 
 class RegisterViewController: UIViewController {
     @IBOutlet var emailField: UITextField!
     @IBOutlet var passwordField: UITextField!
     
+    var ref: DatabaseReference!
+    var uid: String!
     
     @IBAction func registerButtonClicked(_ sender: Any) {
         Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { authResult, error in
             if authResult != nil{
                 print("register success")
+                
+                // Firebase Database 연결
+                self.ref = Database.database().reference()
+                
+                // 데이터베이스에 회원정보 저장 
+                if Auth.auth().currentUser != nil {
+                    let user = Auth.auth().currentUser
+                    self.uid = user?.uid
+                    
+                    let value: [String: Any] = [ "uid" : self.uid, "email" : user?.email]
+                    self.ref.child("Users").child(self.uid).setValue(value)
+                } else{
+                    print("user is nil")
+                }
+                
                 guard let nextVC = self.storyboard?.instantiateViewController(identifier: "HomeVC") as? ViewController else {return}
               
                 self.navigationController?.pushViewController(nextVC, animated: true)
@@ -24,5 +42,6 @@ class RegisterViewController: UIViewController {
                 print("register failed")
             }
         }
+        
     }
 }
