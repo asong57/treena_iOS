31f58e000b74c9c0f60aee8e49db77c41eb17714
asong57 <1998thddk@gmail.com>
2021-09-20 14:34:41 +0900

    feat : 달력 클릭 이벤트

--- treena/treena/Calendar.swift
+++ treena/treena/Calendar.swift
@@ -7,7 +7,47 @@
 
 import Foundation
 import UIKit
+import FSCalendar
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
     
+    var calendar = FSCalendar()
+    var dates: [Date] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set delegate & datasource
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        view.addSubview(calendar)
+    }
+    
+    
+
+    
+    // 달력 날짜 아래에 이미지 셋
+    // FSCalendarDataSource
+    func calendar(_ calendar: FSCalendar!, imageFor date: NSDate!) -> UIImage! {
+        var anyImage: UIImage! = UIImage(named: "leaf2")
+        
+        
+        return anyImage
+    }
+}
+
+// 달력 클릭이벤트
+extension CalendarViewController {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "CalendarDiaryVC") as? CalendarDiaryViewController else {return}
+        
+        // 날짜를 원하는 형식으로 저장하기 위한 방법입니다.
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        nextVC.date = dateFormatter.string(from: date)
+        
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
 }
---
+++ treena/treena/CalendarDiary.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarDiary.swift
+//  treena
+//
+//  Created by asong on 2021/09/20.
+//
+
+import Foundation
+import UIKit
+
+class CalendarDiaryViewController: UIViewController {
+    var date: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(date)
+       
+    }
+    
+}
