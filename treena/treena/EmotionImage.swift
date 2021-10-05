379bc921491be3b382f87560bc6fbdc33c9b7144
asong57 <1998thddk@gmail.com>
2021-10-05 11:40:23 +0900

    feat : 일기 감정에 맞는 멘트 세팅 완료 및 랜덤 난수 활용 추가

--- treena/treena/EmotionImage.swift
+++ treena/treena/EmotionImage.swift
@@ -13,6 +13,7 @@ import FirebaseAuth
 class EmotionImageViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var mentTextView: UITextView!
     
     var emotion: String!
     var ref: DatabaseReference!
@@ -58,12 +59,16 @@ class EmotionImageViewController: UIViewController {
         
     }
     
-    // 감정 타입 세팅
+    // 감정 타입 세팅 및 멘트 세팅
     func setEmotionType(){
+        var random: Int
         for i in 0..<emotionResults.emotions.capacity {
             if emotion == emotionResults.emotions[i].name {
                 id = i
                 type = emotionResults.emotions[i].type
+                random = Int(arc4random_uniform(UInt32(emotionResults.emotions[i].response.capacity)))
+                mentTextView.text = emotionResults.emotions[i].response[random]
+                mentTextView.textColor = UIColor.black
                 print("response : \(emotionResults.emotions[i].response[0])")
                 print("type : \(type)")
                 break
@@ -130,10 +135,13 @@ class EmotionImageViewController: UIViewController {
                                          ["https://postfiles.pstatic.net/MjAyMTA3MzBfMjIw/MDAxNjI3NTcxNDg0MzAz.WVfCnnI6UPKSbpWDJMl8Evblz0w9LpcS1YqaCEF4omUg.Mn2AzXj_54A_GmEdehcM0V8IAQA4v5BzbYimoGc7VU0g.JPEG.hahahafb/water1.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMjU1/MDAxNjI3NTcxNDg0MzY2.aJjahQlHICwJQAAlrj_CHQs2A5YDd_cml-YXnPAQ3BIg.SIg_dZrLJMkumNPNizRenAX07XeQmZiy0FGzRxhH_zYg.JPEG.hahahafb/water2.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMTI0/MDAxNjI3NTcxNDg0Mzk2.wsk6iF5tCSd8G1giAYxgA_ftcuFpPTPg-Fq0HvE5xZMg.qu_H8hmizKClwAPqQIXDx4WnP7XZW4TRlLrFb7ZDrc0g.JPEG.hahahafb/water3.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMjIy/MDAxNjI3NTcxNDg0NDI5.bi7C9SYh2wyLf3DhcHRVJCgWdZA35tpUCov8jBgGe1kg.x1dIDyGPMeUjKxarDw_7zEWu76WGk2Wi_wvxV40n8fIg.JPEG.hahahafb/water4.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMzgg/MDAxNjI3NTcxNDg0NDA2.RW5b_LBffABU-LUlwfH9acgn61gSToBpGW9H4DVWKDYg.C1DiQmYqjH3Hl7I3ygNuFeeGZH41NQKrXnisHZZfgTAg.JPEG.hahahafb/water5.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMjcw/MDAxNjI3NTcxNDg0NDI0.TwhdEhuxC0MWFvtC_rgoMeLdG2Wokj-lKRPnH1Ys390g.hq5xvrlll8BaaLsD-Tfp_vR2rAJ2w0mtrQskpRh53kgg.JPEG.hahahafb/water6.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMjA1/MDAxNjI3NTcxNDg0NzMy.1iSdhB-igYqEQRD-c1ioKjQk5B-Ag34vyRsjN9f6j6Qg.oa3xTW9I5SjsRcLJSvJHw9r1Z_xWFyJsmc4Wx-sSVF8g.JPEG.hahahafb/water7.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMTY4/MDAxNjI3NTcxNDg0ODQ1.DGR-9LTPgR_xeBx5V5HvpBTdcFJ9rmLsZ98qt9N5V_4g.MM-Mi84l-q2BlyltcoDFVdDUXgrX7S1q4j8qcpZ0Rj4g.JPEG.hahahafb/water8.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MzBfMTE1/MDAxNjI3NTcxNDg0OTIw.3Da2QTE0cUxnXw8MFOeVqfeij2q4szvw13VY80wXrUIg.37I-2kDjbSmGjOJKUeknq2D3DqvHUwADJRXJRtRdKW4g.JPEG.hahahafb/water9.jpg?type=w966"],
                                          ["https://postfiles.pstatic.net/MjAyMTA3MjlfMTM0/MDAxNjI3NTY5NTgzMzM1.N1rBudltVY-kUPF16sbEeMtM-OMA2Yu2AGEoGrhcDAQg.zND4wWN-NdRpDc52t8SQTMeChA2dsduYQB3lNpzUHQEg.JPEG.hahahafb/rainny1.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfMjg1/MDAxNjI3NTY5NTgzNDY0.OyjJ8UiJ-hZzNmY9RNMdGahEoWt4wjRtqjrfXFtxgPQg.4bF8C5fs5iESAZK9zDX0xtIexGOPezAhprRmLXYo1WAg.JPEG.hahahafb/rainny2.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfMTAx/MDAxNjI3NTY5NTgzNDQ4.UUS5pmg-ZBV-pP13n7pQXFVmawVZtT0Qi_GkG0btDzog.dfuyUJkWGDgnmvF5zCpjwzZgqLgNueR8OXb1gIFeyiMg.JPEG.hahahafb/rainny3.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfMjky/MDAxNjI3NTY5NTgzMzk3.bjeuZ_yRQCRhtYWrVfB8P2aTtHnX4lqc3iCnBZnPM9Mg.Yr4XjYjG691K4CH7zpgpc9La_OfJq8jS3aGVWMT6Tqkg.JPEG.hahahafb/rainny4.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfOTUg/MDAxNjI3NTcwNTM3NzUz.v7Pd36DojPk4QMjQzi8xxFhB3P__9jlgC7W6TFWL0Yog.qwPM9oDCVsKU7l7oOPXlPR3wSBTaM18NlDkTKIocwA4g.JPEG.hahahafb/rainny5.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfMTY4/MDAxNjI3NTY5NTgzNDI4.HnN3Wll8fE58yGmZ3S5Jr37rbM5779kvt27M6EmY4dYg.6qdKZ8GWVKE80KpgdCeiUxmxhDkjfgw31Ce-_OFioUQg.JPEG.hahahafb/rainny6.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfMjAg/MDAxNjI3NTY5NTgzNjc1.8h5cOlIjQJD7-9uLh1YfD8eJ-JewSsaUwnynFGSb9pIg.d3i5peSlL1LfoRYAxK1VHotOstZsmd2tq31oDfsaqS4g.JPEG.hahahafb/rainny7.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfMjIg/MDAxNjI3NTY5NTgzODI4.X5XcwM-Gc4T3LS-0j866NRXLNre7tnQIdCX0dUmaS_8g.PEklfFGQeNYNW1WgWcHJhDe0pSQZk-cbKwR3t-eCAlog.JPEG.hahahafb/rainny8.jpg?type=w966","https://postfiles.pstatic.net/MjAyMTA3MjlfNjQg/MDAxNjI3NTY5NTgzOTE5.-D-zuZeRBwC4MyH38C9D8ZDIrqMcz4V78fdRI1vueTUg.nxVJTRFQ8r8fjMVfIcEc-9uKF07Qsy7S4OG977WGS8og.JPEG.hahahafb/rainny9.jpg?type=w966"]];
         var index: Int = 0
+        var random: Int
         if type == 1 {
-            index = 1
+            random = Int(arc4random_uniform(UInt32(3)))
+            index = random
         } else if type == 0 {
-            index = 3
+            random = Int(arc4random_uniform(UInt32(2)))
+            index = random+3
         } else if type == -1 {
             index = 5
         }
